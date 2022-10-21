3bfbf80cba99c35384191e0d151ed2e420759b0e
devilb2103 <devbhanushali21@gmail.com>
2022-10-21 21:55:32 +0530

    changed title

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Voting Dapp",
       debugShowCheckedModeBanner: false,
       home: Home(),
     );
